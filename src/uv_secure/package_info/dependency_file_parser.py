7be5e471da04a3b60af7af1ed2b726e809843035
Owen Lamont <owenrlamont@gmail.com>
2025-02-15 22:22:41 +1030

    Adding logic to identify first order dependencies

--- src/uv_secure/package_info/dependency_file_parser.py
+++ src/uv_secure/package_info/dependency_file_parser.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Optional
 
 from anyio import Path
 from pydantic import BaseModel
@@ -14,6 +15,7 @@ else:
 class Dependency(BaseModel):
     name: str
     version: str
+    first_order: bool = False
 
 
 async def parse_requirements_txt_file(file_path: Path) -> list[Dependency]:
@@ -31,11 +33,18 @@ async def parse_requirements_txt_file(file_path: Path) -> list[Dependency]:
         )
         return []
     dependencies = []
+    dependency: Optional[Dependency] = None
     for line in lines:
-        if "==" not in line:
-            continue
-        name, version = line.split("==")
-        dependencies.append(Dependency(name=name.strip(), version=version.strip()))
+        if "==" in line:
+            if dependency is not None:
+                dependencies.append(dependency)
+                dependency = None
+            name, version = line.split("==")
+            dependency = Dependency(name=name.strip(), version=version.strip())
+        elif (" -r " in line or " (pyproject.toml)" in line) and dependency is not None:
+            dependency.first_order = True
+    if dependency is not None:
+        dependencies.append(dependency)
     return dependencies
 
 
@@ -43,9 +52,24 @@ async def parse_uv_lock_file(file_path: Path) -> list[Dependency]:
     """Parses a uv.lock TOML file and extracts package PyPi dependencies"""
     data = toml.loads(await file_path.read_text())
 
+    first_order_dependencies: set[str] = set()
+    dependencies = {}
     package_data = data.get("package", [])
-    return [
-        Dependency(name=package["name"], version=package["version"])
-        for package in package_data
-        if package.get("source", {}).get("registry") == "https://pypi.org/simple"
-    ]
+    for package in package_data:
+        source = package.get("source", {})
+        if source.get("registry") == "https://pypi.org/simple":
+            dependencies[package["name"]] = Dependency(
+                name=package["name"], version=package["version"]
+            )
+        elif source.get("editable") == "." or source.get("editable") == ".":
+            for dependency in package.get("dependencies", []):
+                first_order_dependencies.add(dependency["name"])
+            for group in source.get("dev-dependencies", {}):
+                for group_dependencies in group.values():
+                    for dependency in group_dependencies:
+                        first_order_dependencies.add(dependency["name"])
+
+    dependency_list = list(dependencies.values())
+    for dependency in dependency_list:
+        dependency.first_order = dependency.name in first_order_dependencies
+    return dependency_list
